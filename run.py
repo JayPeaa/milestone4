bc09b6327adef5159f38c8ce14fea7d03c6b703e
John Hay <john.paul.hay@outlook.com>
2019-05-24 14:09:41 +0000

    Create MongoDB and Link with Pythonscript to run.py

--- run.py
+++ run.py
@@ -1,5 +1,27 @@
+import pymongo
 import os
 from flask import Flask, render_template
+
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "cook-e"
+COLLECTION_NAME = "recipes"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected!")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to Mongo: %s") % e 
+        
+conn = mongo_connect(MONGODB_URI)
+
+coll = conn[DBS_NAME][COLLECTION_NAME]
+
+documents = coll.find()
+
+for doc in documents:
+    print(doc)
 
 app = Flask(__name__)
 

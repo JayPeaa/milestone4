abefc458feae90920e51f250f46c550ede073db5
John Hay <john.paul.hay@outlook.com>
2019-05-27 07:36:27 +0000

    Add category cards to recipe tab and remove about page as not required

--- run.py
+++ run.py
@@ -29,10 +29,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
 @app.route("/contact")
 def contact():
     return render_template("contact.html")

e58d21d11fef2ab37c7c205241b70426d9e10c11
John Hay <john.paul.hay@outlook.com>
2019-05-28 08:44:34 +0000

    Darken background image and add content to home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-        direction: 'down',
-        hoverEnabled: false
-    });
-});
+/* Vanilla Javascript */
+
+
+
+/* Jquery */
+/* Mobile Navigation Class */
+
+ $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });

4813c741c878c034162bd825c25103e0c70718ce
John Hay <john.paul.hay@outlook.com>
2019-07-25 08:48:34 +0000

    Fix registration error and add sign in text on registration page

--- run.py
+++ run.py
@@ -255,8 +255,8 @@ def register():
     '''
     if request.method == 'POST':
         users = user_coll
-        existing_user = users.find_one
-        ({'user_name': request.form['user_name']})
+        existing_user = users.find_one(
+            {'user_name': request.form['user_name']})
 
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'),
